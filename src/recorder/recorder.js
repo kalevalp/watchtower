51ca244e701c6b0eb6ed29c6669e4d56089fc516
Kalev Alpernas <kalev.alp@gmail.com>
2018-08-14 17:13:50 -0700

    Generified the recorder functions - each may now get an arbitrary number of parameters.

--- src/recorder/recorder.js
+++ src/recorder/recorder.js
@@ -11,57 +11,51 @@ const conf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
 const unsecuredLambda = fs.readFileSync(conf.unsecLambda, 'utf8');
 
 
-function recordWrapperCallback (call) {
-    return (param, callback) => {
+function recordWrapperSync (call) {
+    return function () {
         console.log("#### Call log");
-        console.log(JSON.stringify(param));
+        console.log(JSON.stringify(arguments));
         console.log("#### END Call log");
 
-        return call(param, (err, data) => {
+        const res = call(...arguments);
+
+        console.log("#### Result log");
+        console.log(JSON.stringify(res));
+        console.log("#### END Result log");
+
+        return res;
+    }
+}
+
+function recordWrapperCallback (call) {
+    return function () {
+        console.log("#### Call log");
+        console.log(JSON.stringify(arguments)); // JSON.stringify returns a JSON representation of the object - i.e. no functions.
+        console.log("#### END Call log");
+
+        const cb = arguments[arguments.length-1];
+
+        arguments[arguments.length-1] = (err, data) => {
 
             console.log("#### Response log");
             console.log(JSON.stringify(err));
             console.log(JSON.stringify(data));
             console.log("#### END Response log");
 
-            callback(err, data)
+            cb(err, data)
 
-        })
+        };
+
+        return call(...arguments);
     }
 }
 function recordWrapperPromise (call) {
-    return (param) => {
+    return function () {
         console.log("#### Call log");
-        console.log(JSON.stringify(param));
+        console.log(JSON.stringify(arguments));
         console.log("#### END Call log");
 
-        return call(param)
-            .then((data) => {
-
-                console.log("#### Response log");
-                console.log(JSON.stringify(data));
-                console.log("#### END Response log");
-
-                return data;
-            })
-            .catch((err) => {
-                console.log("#### Error response log");
-                console.log(JSON.stringify(err));
-                console.log("#### END Error response log");
-
-                return Promise.reject(err);
-            });
-    }
-}
-
-function recordWrapperPromiseTwoParams (call) {
-    return (fst, snd) => {
-        console.log("#### Call log");
-        console.log(JSON.stringify(fst));
-        console.log(JSON.stringify(snd));
-        console.log("#### END Call log");
-
-        return call(fst, snd)
+        return call(...arguments)
             .then((data) => {
 
                 console.log("#### Response log");
@@ -109,6 +103,7 @@ module.exports.${handlerName}(externalEvent, externalContext, externalCallback);
                     externalEvent: event,
                     externalContext: context,
                     externalCallback: callback,
+                    Math : new Proxy(Math, {get: (target, p) => p==="random" ? recordWrapperSync(Math.random()) : target[p]}),
                 },
                 require: {
                     context: 'sandbox',
@@ -155,7 +150,7 @@ module.exports.${handlerName}(externalEvent, externalContext, externalCallback);
                             getTestMessageUrl: recordWrapperPromise(nodemailer.getTestMessageUrl),
                         },
                         'got' : {
-                            get: recordWrapperPromiseTwoParams(got.get),
+                            get: recordWrapperPromise(got.get),
                         },
                         'node-fetch' : recordWrapperPromise(fetch)
                     }
