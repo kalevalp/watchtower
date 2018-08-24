d8a11e9fc47762d80be3eb58c401cc1d9ef9b0d7
Kalev Alpernas <kalev.alp@gmail.com>
2018-08-24 16:21:18 -0700

    examples

--- examples/hello-world/recorded/replayer-handler.js
+++ examples/hello-world/recorded/replayer-handler.js
@@ -1,2 +1,7 @@
-module.exports = require('replayer');
+const replayer = require('replayer');
+
+replayer.replay ( '/aws/lambda/hello-world-recorded-dev-hello',
+    '2018/08/20/[$LATEST]f18b94a01da2400085ce026912702ec8',
+    "4cabcdff-a4ba-11e8-aaeb-d37728f6aa8f");
+
 
