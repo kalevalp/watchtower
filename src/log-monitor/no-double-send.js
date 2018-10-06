2706ffe5531186eeb861b69a615746a70f6f97b3
Kalev Alpernas <kalev.alp@gmail.com>
2018-10-05 17:50:55 -0700

    no double send monitor instance

--- src/log-monitor/no-double-send.js
+++ src/log-monitor/no-double-send.js
@@ -1,14 +1,37 @@
 const monitor = require('./monitor');
 const aws = require('aws-sdk');
-
+const ddb = new aws.DynamoDB();
 const property = {};
 const matchSendRE = /^SEND([A-Za-z0-9.\-_]*@[A-Za-z0-9.\-_]*)##(.*)SEND$/;
+const propertyTable = process.env.PROPERTY_TABLE;
+
 
 property.event = (update) => {
     const sendEvent = update.match(matchSendRE);
     if (sendEvent) {
         const address = sendEvent[1];
         const email = sendEvent[2];
+
+        const params = {
+            Item: {
+                address: address,
+                email: email,
+            },
+            TableName: propertyTable,
+            ReturnValues: 'ALL_OLD',
+        };
+
+        // TODO: callback/promisify
+        ddb.putItem(params, (err, data) => {
+            if (err) {
+
+            }// Do something
+            else {
+                if (data.Attributes !== {}) {
+                    // Violation!
+                }
+            }
+        });
     }
 };
 
