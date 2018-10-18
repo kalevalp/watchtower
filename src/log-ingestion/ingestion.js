e1ac78d0eceb057999a7bf8e3b60c13dc7bf4ae4
Kalev Alpernas <kalev.alp@gmail.com>
2018-10-18 16:44:39 -0700

    event ingestion code

---
+++ src/log-ingestion/ingestion.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const zlib = require('zlib');
+const aws = require('aws-sdk');
+
+const ddb = new aws.DynamoDB();
+const tableName = 'monitoredEvents';
+
+const eventUpdateRE = /\t#####EVENTUPDATE\[(([A-Za-z0-9\-_]+)\(([A-Za-z0-9\-_,]*)\))]#####\n$/;
+
+module.exports.handler = async function(event, context) {
+    const payload = new Buffer(event.awslogs.data, 'base64');
+
+    let logBatch = JSON.parse(zlib.gunzipSync(payload).toString('ascii'));
+    const logEvents = logBatch.logEvents;
+
+    const entries = [];
+
+    for (const logEvent of logEvents) {
+        const eventUpdate = logEvent.message.match(eventUpdateRE);
+        if (eventUpdate) {
+            const eventType = eventUpdate[2];
+            const eventParams = eventUpdate[3].split(',');
+            const entry = {
+                type: eventType, // Partition Key
+                id: logEvent.id.toString(), // Sort Key
+                params: eventParams,
+                timestamp: logEvent.timestamp.toString(),
+                logGroup: logBatch.logGroup,
+                logStream: logBatch.logStream,
+            };
+            entries.push(entry);
+        }
+    }
+
+    const batchedEntries = [];
+
+    for (let i = 0; i < entries.length; i += 25) {
+        batchedEntries.push(entries.slice(i,i+25));
+    }
+
+    console.log(batchedEntries);
+
+    const calls = [];
+
+    for (const batch of batchedEntries) {
+
+        const params = {};
+
+        params.RequestItems = {};
+        params.RequestItems[tableName] = [];
+
+
+        for (const item of batch) {
+            const putRequest = {
+                PutRequest: {
+                    Item: {
+                        "type":      { S:  item.type },
+                        "id":        { S:  item.id },
+                        "params":    { SS: item.params},
+                        "timestamp": { N:  item.timestamp},
+                        "logGroup":  { S:  item.logGroup},
+                        "logStream": { S:  item.logStream},
+                    }
+                }
+            };
+            params.RequestItems[tableName].push(putRequest);
+        }
+        calls.push(ddb.batchWriteItem(params).promise());
+    }
+
+    return Promise.all(calls);
+
+};
