b245cc67e4740c683332a777c19eca5e6082973d
Kalev Alpernas <kalev.alp@gmail.com>
2018-10-19 17:43:23 -0700

    no-double-send monitor.

--- src/log-ingestion/no-double-send.js
+++ src/log-ingestion/no-double-send.js
@@ -1,40 +1,101 @@
-const monitor = require('./monitor');
+/* ****************************************************************************
+ *
+ * Safety property:
+ *   The same email is not sent to the same address twice.
+ *
+ *   \forall{a: address, e: email} : sent(a, e) => X G ( ! sent(a, e) )
+ *
+ *   State Machine:
+ *   Quantified (instantiated?) over a: address, e: email
+ *
+ *   ->() ---a,e---> () ---a,e---> (())
+ *
+ *************************************************************************** */
+
+/* ****************************************************************************
+ *
+ * This variant of the property is *not* instantiated. It reads all relevant
+ * events from the database, and looks for a violation.
+ *
+ *************************************************************************** */
+
+
 const aws = require('aws-sdk');
 const ddb = new aws.DynamoDB();
-const property = {};
-const matchSendRE = /^SEND([A-Za-z0-9.\-_]*@[A-Za-z0-9.\-_]*)##(.*)SEND$/;
-const propertyTable = process.env.PROPERTY_TABLE;
+// const property = {};
+// const matchSendRE = /^SEND([A-Za-z0-9.\-_]*@[A-Za-z0-9.\-_]*)##(.*)SEND$/;
+// const propertyTable = process.env.PROPERTY_TABLE;
+
+const events = ['SEND'];
+// const instance = {
+//     a: '',
+//     b: '',
+// }
+
+function getEvents (collectedEvents, params) {
+    return ddb.query(params).promise()
+        .then(data => {
+            if (data.LastEvaluatedKey) {
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                return getEvents(collectedEvents.concat(data.Items), params)
+            } else {
+                return collectedEvents.concat(data.Items)
+            }
+        })
+}
 
 
-property.event = (update) => {
-    const sendEvent = update.match(matchSendRE);
-    if (sendEvent) {
-        const address = sendEvent[1];
-        const email = sendEvent[2];
+module.exports.handler = async function(event, context) {
 
-        const params = {
-            Item: {
-                address: address,
-                email: email,
-            },
-            TableName: propertyTable,
-            ReturnValues: 'ALL_OLD',
+    const events = [];
+
+    for (const event of events) {
+        const queryRequest = {
+            TableName: process.env.EVENTS_TABLE,
+            KeyConditionExpression: `type = :${event}`,
         };
 
-        // TODO: callback/promisify
-        ddb.putItem(params, (err, data) => {
-            if (err) {
-
-            }// Do something
-            else {
-                if (Object.keys(data.Attributes) !== 0) {
-                    // Violation!
-                }
-            }
-        });
+        events.push(getEvents([], queryRequest));
     }
+
+    Promise.all(events)
+        .then(results => [].concat(...results)) // Return a single array consisting of all events.
+        .then(results => results.sort((a, b) => a.timestamp - b.timestamp)) // Sort by timestamp
+
 };
 
-monitor.setProperty(property);
 
-module.exports.handler = monitor.handler;
+
+
+// property.event = (update) => {
+//     const sendEvent = update.match(matchSendRE);
+//     if (sendEvent) {
+//         const address = sendEvent[1];
+//         const email = sendEvent[2];
+//
+//         const params = {
+//             Item: {
+//                 address: address,
+//                 email: email,
+//             },
+//             TableName: propertyTable,
+//             ReturnValues: 'ALL_OLD',
+//         };
+//
+//         // TODO: callback/promisify
+//         ddb.putItem(params, (err, data) => {
+//             if (err) {
+//
+//             }// Do something
+//             else {
+//                 if (Object.keys(data.Attributes) !== 0) {
+//                     // Violation!
+//                 }
+//             }
+//         });
+//     }
+// };
+//
+// monitor.setProperty(property);
+//
+// module.exports.handler = monitor.handler;
