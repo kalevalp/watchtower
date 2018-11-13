0c9399e676feb6ad8a63056baaa0dca1168c5ae6
Kalev Alpernas <kalev.alp@gmail.com>
2018-11-12 16:28:26 -0800

    Reworked and simplified recorded for the monitoring project.

--- src/recorder/recorder.js
+++ src/recorder/recorder.js
@@ -2,157 +2,35 @@
 
 const {NodeVM,VMScript} = require("vm2");
 const fs = require("fs");
-const aws = require("aws-sdk");
-const nodemailer = require("nodemailer");
-const got = require('got');
-const fetch = require('node-fetch');
-const {recordWrapperSync, recordWrapperCallback, recordWrapperPromise, flushCounters} = require("rnr-utils");
 
-const conf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
-const originalLambda = fs.readFileSync(conf.originalLambda, 'utf8');
+const originalLambdaFile    = process.env['ORIGINAL_LAMBDA_FILE'];
+const originalLambdaHandler = process.env['ORIGINAL_LAMBDA_HANDLER'];
+const originalLambdaPath    = `/var/task/${originalLambdaFile}`;
+// const originalLambdaPath    = `${__dirname}/${originalLambdaFile}`;
 
-const originalLambdaScript = new VMScript(`
-//  ***********************************
-//  ** Original Lambda Code:
-${originalLambda}
-//  ** End of Original Lambda Code:
-//  ***********************************
-        `);
+const originalLambdaCode    = fs.readFileSync(originalLambdaFile, 'utf8');
 
+const originalLambdaScript  = new VMScript(originalLambdaCode);
 
-const processEnv = {};
-for (let envVar of conf.processEnv) {
-    processEnv[envVar] = process.env[envVar];
-}
+function createRecordingHandler(module, mock) {
 
-let executionEnv = {
-    console: 'inherit',
-    sandbox: {
-        process: {
-            env: processEnv,
+    let executionEnv = {
+        console: 'inherit',
+        sandbox: {
+            process: process,
         },
-        // externalEvent: event,
-        // externalContext: context,
-        // externalCallback: callback,
-        Math : new Proxy(Math, {get: (target, p) => p==="random" ? recordWrapperSync(Math.random,"Math.random") : target[p]}),
-    },
-    require: {
-        context: 'sandbox',
-        external: true,
-        builtin: ['*'],
-        root: "./",
-        mock: {
-            'aws-sdk': {
-                config: aws.config,
+        require: {
+            context: 'sandbox',
+            external: true,
+            builtin: ['*'],
+            root: "./",
+            mock: mock,
+        },
+    };
 
-                Kinesis: function () {
-                    const kinesis = new aws.Kinesis();
-                    return {
-                        putRecord: recordWrapperCallback(kinesis.putRecord, "aws-sdk.Kinesis.putRecord"),
-                    }
-                },
+    const vm = new NodeVM(executionEnv);
 
-                StepFunctions: function () {
-                    const stepfunctions = new aws.StepFunctions();
-                    return {
-                        startExecution: recordWrapperCallback(stepfunctions.startExecution, "aws-sdk.StepFunctions.startExecution"),
-                        getActivityTask: recordWrapperCallback(stepfunctions.getActivityTask, "aws-sdk.StepFunctions.getActivityTask"),
-                        sendTaskFailure: recordWrapperCallback(stepfunctions.sendTaskFailure, "aws-sdk.StepFunctions.sendTaskFailure"),
-                        sendTaskSuccess: recordWrapperCallback(stepfunctions.sendTaskSuccess, "aws-sdk.StepFunctions.sendTaskSuccess"),
-                    }
-                },
+    const vmExports = vm.run(originalLambdaScript, originalLambdaPath);
 
-                Rekognition: function () {
-                    const rek = new aws.Rekognition();
-
-                    return {
-                        detectLabels: recordWrapperCallback((params, cb) => rek.detectLabels(params, cb), "aws-sdk.Rekognition.detectLabels"),
-                    }
-                },
-                S3: function () {
-                    const s3 = new aws.S3();
-                    return new Proxy(s3, {get: (target, p) =>
-                            p==="putObject" ? (params, callback) => {
-                                    params.Tagging = params.Tagging ? params.Tagging + "&" : "";
-
-                                    params.Tagging = params.Tagging + "execid=" + invocationID;
-
-                                    return recordWrapperCallback((params, cb) => s3.putObject(params, cb),"aws-sdk.s3.putobject")(params, callback);
-                                } :
-                                p==="getObject" ? recordWrapperCallback((params, cb) => s3.getObject(params, cb),"aws-sdk.S3.getObject") :
-                                    target[p]});
-                },
-                DynamoDB: function (options) {
-                    const ddb = new aws.DynamoDB(options);
-                    return new Proxy(ddb, {get: (target, p) =>
-                            p==="putItem" ?
-                                (params, callback) => {
-                                    params.Item.execId = invocationID;
-
-                                    return recordWrapperCallback((params, cb) => target.putItem(params, cb), "aws-sdk.dynamodb.putItem")(params, callback);
-                                } :
-                                p==="getItem" ?
-                                    (params, callback) => {
-                                        return recordWrapperCallback((ps, cb) => target.getItem(ps, cb))(params, callback)
-                                    } :
-                                target[p]
-                    })
-
-                }
-            },
-            'nodemailer' : {
-                createTransport: (params) => {
-                    const mailer = nodemailer.createTransport(params);
-
-                    return {
-                        sendMail: recordWrapperPromise(mailer.sendMail, "nodemailer.sendMail"),
-                    }
-                },
-                getTestMessageUrl: recordWrapperPromise(nodemailer.getTestMessageUrl,"nodemailer.getTestMessageUrl"),
-            },
-            'got' : {
-                get: recordWrapperPromise(got.get, "got.get"),
-            },
-            'node-fetch' : recordWrapperPromise(fetch, "node-fetch.fetch")
-        }
-    },
-};
-
-const vm = new NodeVM(executionEnv);
-
-const vmExports = vm.run(originalLambdaScript, conf.secLambdaFullPath);
-
-let invocationID;
-
-for (let handlerName of conf.handlers) {
-    module.exports[handlerName] = function (event, context, callback) {
-        flushCounters();
-
-        invocationID = context.invokeid;
-
-        console.log("Recording Execution Context.");
-
-        console.log("#### EVENT ####");
-        console.log(JSON.stringify(event));
-        console.log("#### EVENTEND ####");
-
-        console.log("#### CONTEXT ####");
-        console.log(JSON.stringify(context));
-        console.log("#### CONTEXTEND ####");
-
-        vmExports[handlerName](event,context,(err, data) => {
-
-            console.log("Recording Callback.");
-
-            console.log("#### CBACKERR ####");
-            console.log(JSON.stringify(err));
-            console.log("#### CBACKERREND ####");
-
-            console.log("#### CBACKDATA ####");
-            console.log(JSON.stringify(data));
-            console.log("#### CBACKDATAEND ####");
-
-            callback(err,data);
-        });
-    }
+    module.exports[originalLambdaHandler] = vmExports[originalLambdaHandler];
 }
