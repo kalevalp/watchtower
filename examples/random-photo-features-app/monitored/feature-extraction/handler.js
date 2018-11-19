2cdd3e5b757a2befa58b4ee589ec92ef2f9c5ea2
Kalev Alpernas <kalev.alp@gmail.com>
2018-11-19 11:27:33 -0800

    Bugfixes in monitored feature extraction function.

--- examples/random-photo-features-app/monitored/feature-extraction/handler.js
+++ examples/random-photo-features-app/monitored/feature-extraction/handler.js
@@ -7,61 +7,63 @@ const rek = new aws.Rekognition();
 
 const mock = {
     'aws-sdk' : {
-        S3: () => ({
-            getObject: (params) => ({
-                promise: () => s3.getObject(params)
-                    .promise()
-                    .then(data => {
-                        const image = data.Body;
+        S3: function () {
+            return ({
+                getObject: (params) => ({
+                    promise: () => s3.getObject(params)
+                        .promise()
+                        .then(data => {
+                            const image = data.Body;
 
-                        const hash = crypto.createHash('sha256');
-                        hash.update(image.toString());
-                        const imageHash = hash.digest('hex');
+                            const hash = crypto.createHash('sha256');
+                            hash.update(image.toString());
+                            const imageHash = hash.digest('hex');
 
-                        const bucket = params.Bucket;
-                        const objectKey = params.Key;
-                        console.log(`#####EVENTUPDATE[IMAGE_GET(${bucket},${objectKey},${imageHash})]#####`);
-                        return data;
-                    }),
-            }),
-            putObject: (params) => ({
-                promise: () => s3.getObject(params)
-                    .promise()
-                    .then(data => {
-                        const features = params.Body;
+                            const bucket = params.Bucket;
+                            const objectKey = params.Key;
+                            console.log(`#####EVENTUPDATE[IMAGE_GET(${bucket},${objectKey},${imageHash})]#####`);
+                            return data;
+                        }),
+                }),
+                putObject: (params) => ({
+                    promise: () => s3.putObject(params)
+                        .promise()
+                        .then(data => {
+                            const features = params.Body;
 
-                        const hash = crypto.createHash('sha256');
-                        hash.update(features.toString());
-                        const featuresHash = hash.digest('hex');
+                            const hash = crypto.createHash('sha256');
+                            hash.update(features.toString());
+                            const featuresHash = hash.digest('hex');
 
-                        const bucket = params.Bucket;
-                        const objectKey = params.Key;
-                        console.log(`#####EVENTUPDATE[FEATURES_PUT(${bucket},${objectKey},${featuresHash})]#####`);
-                        return data;
-                    }),
-            }),
-        }),
-        Rekognition: () => ({
-            detectLabels: (params) => ({
-                promise: () => rek.detectLabels(params)
-                    .promise()
-                    .then(data => {
-                        const image = params.Image.Bytes;
+                            const bucket = params.Bucket;
+                            const objectKey = params.Key;
+                            console.log(`#####EVENTUPDATE[FEATURES_PUT(${bucket},${objectKey},${featuresHash})]#####`);
+                            return data;
+                        }),
+                }),
+            })},
+        Rekognition: function () {
+            return ({
+                detectLabels: (params) => ({
+                    promise: () => rek.detectLabels(params)
+                        .promise()
+                        .then(data => {
+                            const image = params.Image.Bytes;
 
-                        let hash = crypto.createHash('sha256');
-                        hash.update(image.toString());
-                        const imageHash = hash.digest('hex');
+                            let hash = crypto.createHash('sha256');
+                            hash.update(image.toString());
+                            const imageHash = hash.digest('hex');
 
-                        hash = crypto.createHash('sha256');
-                        const features = JSON.stringify(data.Labels);
-                        hash.update(features);
-                        const featuresHash = hash.digest('hex');
+                            hash = crypto.createHash('sha256');
+                            const features = JSON.stringify(data.Labels);
+                            hash.update(features);
+                            const featuresHash = hash.digest('hex');
 
-                        console.log(`#####EVENTUPDATE[DETECT_LABELS(${imageHash},${featuresHash})]#####`);
-                        return data;
-                    }),
-            }),
-        }),
+                            console.log(`#####EVENTUPDATE[DETECT_LABELS(${imageHash},${featuresHash})]#####`);
+                            return data;
+                        }),
+                }),
+            })},
     }
 };
 
