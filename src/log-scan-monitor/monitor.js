0338fc8cfaad2268c6aa80feb951110c41650066
Kalev Alpernas <kalev.alp@gmail.com>
2018-11-26 15:34:32 -0800

    Modified monitor to run for a specific property.

--- src/log-scan-monitor/monitor.js
+++ src/log-scan-monitor/monitor.js
@@ -1,14 +1,12 @@
 const aws = require('aws-sdk');
 const ddb = new aws.DynamoDB();
 
-const propertyInstances = {};
-
 function getEvents (collectedEvents, params) {
     return ddb.query(params).promise()
         .then(data => {
-            if (data.LastEvaluatedKey) {
+            if (data.LastEvaluatedKey) { // There are additional items in DynamoDB
                 params.ExclusiveStartKey = data.LastEvaluatedKey;
-                return getEvents(collectedEvents.concat(data.Items), params)
+                return getEvents(collectedEvents.concat(data.Items), params);
             } else {
                 return collectedEvents.concat(data.Items)
             }
@@ -16,16 +14,26 @@ function getEvents (collectedEvents, params) {
 }
 
 
-module.exports.monitorFactory = (instantiateProperty) => {
-
-    return async function(event, context) {
+module.exports.monitorFactory = (tableName, prop) => {
+    return async function(event) {
+        const instance = event.data.instantiatedVars;
 
         const ddbCalls = [];
 
-        for (const event of events) {
+        for (const proj of prop.projections) {
+            let propinstKey = prop.name;
+
+            for (const qvar of proj) {
+                if (! instance[qvar]) {
+                    throw `Instance is missing an assignment to quantified variable ${qvar}.`;
+                }
+
+                propinstKey+=qvar;
+                propinstKey+=instance[qvar];
+            }
             const queryRequest = {
-                TableName: process.env.EVENTS_TABLE,
-                KeyConditionExpression: `type = :${event}`,
+                TableName: tableName,
+                KeyConditionExpression: `propinst = :${propinstKey}`,
             };
 
             ddbCalls.push(getEvents([], queryRequest));
@@ -33,16 +41,10 @@ module.exports.monitorFactory = (instantiateProperty) => {
 
         Promise.all(ddbCalls)
             .then(results => [].concat(...results)) // Return a single array consisting of all events.
-            .then(results => results.sort((a, b) => a.timestamp - b.timestamp)) // Sort by timestamp
+            .then(results => results.sort((a, b) => a.timestamp === b.timestamp ? a.id - b.id : a.timestamp - b.timestamp)) // Sort by timestamp, with id being a tie-breakers
             .then(results => {
-                for (let i = 0; i < results.length - 1; i++) {
-                    if (results[i].timestamp === results[i+1].timestamp) {
-                        // TODO: Handle potential race
-                    }
-                }
-                return results;
-            }) // Check for potential races
-            .then(results => {
+                let state = 'INITIAL';
+
                 for (const e of results) {
                     const eventType       = e.type ;
                     // const eventId         = e.id ;
@@ -51,11 +53,19 @@ module.exports.monitorFactory = (instantiateProperty) => {
                     // const eventLogGroup   = e.logGroup;
                     // const eventLogStream  = e.logStream;
 
-                    if (!propertyInstances[eventParams]) {
-                        propertyInstances[eventParams] = instantiateProperty(...eventParams);
-                    }
+                    if (prop.stateMachine[eventType]) {
+                        // Make sure that the constants match
 
-                    propertyInstances[eventParams](eventType);
+                        // Sanity check that the quantified variable assignment matches the current property instance
+
+                        state = prop.stateMachine[eventType].to;
+
+                        if (state === 'FAILURE') {
+                            // Somehow report to the user that the property had been violated.
+                        } else if (state === 'SUCCESS') {
+                            // Terminate execution, and mark property so that it is not checked again.
+                        }
+                    }
                 }
             })
     }
