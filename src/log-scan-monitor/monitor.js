0ca067d9c1c30338589d2a2def04359a94855dab
Kalev Alpernas <kalev.alp@gmail.com>
2018-11-26 17:31:14 -0800

    Added const and instance checks to the monitor.

--- src/log-scan-monitor/monitor.js
+++ src/log-scan-monitor/monitor.js
@@ -1,6 +1,11 @@
 const aws = require('aws-sdk');
 const ddb = new aws.DynamoDB();
 
+const constRE = /const__(.*$)/;
+const varRE = /var__([a-zA-Z0-9_]+)/;
+
+
+
 function getEvents (collectedEvents, params) {
     return ddb.query(params).promise()
         .then(data => {
@@ -53,21 +58,55 @@ module.exports.monitorFactory = (tableName, prop) => {
                     // const eventLogGroup   = e.logGroup;
                     // const eventLogStream  = e.logStream;
 
-                    if (prop.stateMachine[eventType]) {
+                    if (prop.stateMachine[eventType][state]) {
                         // Make sure that the constants match
+                        let constsOk = true;
+                        for (let i = 0; i < prop.stateMachine[eventType][state].params.length; i++) {
+                            let constantsMatch = prop.stateMachine[eventType][state].params[i].match(constRE);
+                            if (constantsMatch) {
+                                const constantValue = constantsMatch[1];
+
+                                if (constantValue !== eventParams[i]) {
+                                    // This is not the event you're looking for
+                                    constsOk = false;
+                                }
+                            }
+                        }
 
                         // Sanity check that the quantified variable assignment matches the current property instance
+                        let instanceOk = true;
+                        for (let i = 0; i < prop.stateMachine[eventType][state].params.length; i++) {
+                            const varnameMatch = prop.stateMachine[eventType][state].params[i].match(varRE);
+                            if (varnameMatch) {
+                                const varname = varnameMatch[1];
+                                if (eventParams[i] !== instance[varname]) {
+                                    // Maybe should throw an error of some kind. This should not happen, ever.
+                                    instanceOk = false;
+                                }
+                            }
+                        }
 
-                        state = prop.stateMachine[eventType].to;
+                        if (constsOk && instanceOk) {
+                            state = prop.stateMachine[eventType][state].to;
 
-                        if (state === 'FAILURE') {
-                            // Somehow report to the user that the property had been violated.
-                        } else if (state === 'SUCCESS') {
-                            // Terminate execution, and mark property so that it is not checked again.
+                            if (state === 'FAILURE') {
+                                // Somehow report to the user that the property had been violated.
+                                // At the moment - fail. TODO: use AWS SES to send an email to someone.
+
+                                // TODO: make a more readable print of the instance.
+                                return `Property ${prop.name} was violated for property instance ${instance}`;
+                            } else if (state === 'SUCCESS') {
+                                // Terminate execution, and mark property so that it is not checked again.
+
+                                return `Property ${prop.name} holds for property instance ${instance}`;
+                            } else {
+                                // No violation found, but it might still be violated depending on future events.
+
+                                return `Property ${prop.name} was not violated (but might be violated by future events) for property instance ${instance}`;
+                            }
                         }
                     }
                 }
             })
     }
-
 };
