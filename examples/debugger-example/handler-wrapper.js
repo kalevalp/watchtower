266f287af948724d9a7c1aa60dabc0ec22dcf1e6
Kalev Alpernas <kalev.alp@gmail.com>
2020-01-29 17:37:44 +0200

    aws-sdk needs to be explicitly required from outside the vm

--- examples/debugger-example/handler-wrapper.js
+++ examples/debugger-example/handler-wrapper.js
@@ -1,12 +1,16 @@
 const recorder = require('watchtower-recorder');
 const rnrBucket = process.env.WATCHTOWER_RNR_S3_BUCKET;
 
+const aws = require('aws-sdk');
+
 let context, lambdaExecutionContext, lambdaInputEvent;
 function updateContext(name, event, lambdaContext) { context = name; lambdaExecutionContext = lambdaContext; lambdaInputEvent = event; }
 
 recorder.configureRNRRecording(/*enable*/ true, /*kinesisStreamName*/ 'notActuallyInUse', /*s3BucketName*/ rnrBucket, /*getContext*/ () => lambdaExecutionContext )
 
-const mock = {};
+const mock = {
+    'aws-sdk': aws
+};
 
-module.exports.consume = recorder.createRecordingHandler('handler.js', 'consume' , mock, updateContext, true);
-module.exports.produce = recorder.createRecordingHandler('handler.js', 'produce' , mock, updateContext, true);
+module.exports.consume = recorder.createRecordingHandler('handler.js', 'consume' , mock, true, updateContext, true);
+module.exports.produce = recorder.createRecordingHandler('handler.js', 'produce' , mock, true, updateContext, true);
