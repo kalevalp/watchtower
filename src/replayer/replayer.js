2b04cb12d0b84e7a3231e8c87a48a94933778b78
Kalev Alpernas <kalev.alp@gmail.com>
2020-01-30 22:34:12 +0200

    Collecting events from s3

--- src/replayer/replayer.js
+++ src/replayer/replayer.js
@@ -1,5 +1,16 @@
 const {NodeVM,VMScript} = require("vm2");
 const fs = require("fs");
+const util = require('util');
+
+const aws = require('aws-sdk');
+// const serialize = require('serialize-javascript-w-cycles');
+const zlib = require('zlib');
+const gunzip = util.promisify(zlib.gunzip);
+
+const kinesis = new aws.Kinesis();
+const s3 = new aws.S3();
+
+const debug = process.env.DEBUG_WATCHTOWER;
 
 function createReplayHandler(originalLambdaFile, originalLambdaHandler, mock, updateContext, useCallbacks = false) {
 
@@ -28,21 +39,48 @@ function createReplayHandler(originalLambdaFile, originalLambdaHandler, mock, up
 
     if (!useCallbacks) {
         return async (event, context) => {
+            debugger;
             return vmExports[originalLambdaHandler](event, context);
         }
     } else {
 	return (event, context, callback) => {
+            debugger;
 	    return vmExports[originalLambdaHandler](event, context, callback);
 	}
     }
 }
 
-async function replayAsyncHandler(executionID, handler) {
+async function replayAsyncHandler(executionID, handler, s3BucketName) {
+    const history = await s3.listObjects({Bucket: s3BucketName, Prefix: executionID}).promise();
+
+    // TODO - handle history larger than 1000 objects
+
+    const hist = await Promise.all((await Promise.all(history.Contents
+                                                      .map(item => item.Key)
+                                                      .map(async Key => (
+                                                          {
+                                                              key: Key,
+                                                              item: await s3.getObject({Key, Bucket: s3BucketName}).promise()
+                                                          }))))
+                                   .map(async ({key, item}) => (
+                                       {
+                                           key,
+                                           item: (await gunzip(item.Body)).toString()
+                                       })))
+
+    // console.log(hist);
+
     // TODO
 }
 
-function replaycbackHandler(executionID, handler) {
+function replayCBackHandler(executionID, handler, s3BucketName) {
     // TODO
 }
 
+module.exports.replayAsyncHandler = replayAsyncHandler;
 
+if (require.main === module) {
+    const execId = process.argv[2];
+
+    replayAsyncHandler(execId,'','wtrnrbucket');
+}
